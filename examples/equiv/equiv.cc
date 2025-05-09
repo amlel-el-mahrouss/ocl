03c81e0d2d37b0ad4b67dbe468851cf2b3f13e2e
Amlal <amlal@nekernel.org>
2025-05-09 13:18:53 +0200

    feat(equiv): improve example for equiv.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- examples/equiv/equiv.cc
+++ examples/equiv/equiv.cc
@@ -10,9 +10,10 @@
 /* finally test it */
 int main(int argc, char** argv)
 {
-	std::cout << std::boolalpha << astdx::is_same<bool, int>::value << std::endl;
-	std::cout << std::boolalpha << astdx::is_same<bool, bool>::value << std::endl;
-	std::cout << std::boolalpha << astdx::is_same<int, int>::value << std::endl;
+	std::cout << std::boolalpha;
+	std::cout << astdx::is_same<bool, int>::value << std::endl;
+	std::cout << astdx::is_same<bool, bool>::value << std::endl;
+	std::cout << astdx::is_same<int, int>::value << std::endl;
 
 	return 0;
 }
