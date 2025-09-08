2ecbd2bfb27060b6f45196d8481fd0ccdbea730c
Amlal El Mahrouss <amlal@nekernel.org>
2025-09-08 23:18:52 +0200

    feat: wip: `simd` module in progress.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- dev/lib/simd/simd.hpp
+++ dev/lib/simd/simd.hpp
@@ -9,10 +9,51 @@
 
 #include <lib/core/includes.hpp>
 
+/// @author Amlal El Mahrouss
+/// @brief Basic SIMD processor.
+
 namespace ocl::snu::simd
 {
-  struct simd_backend;
+	template <typename SimdBackend>
+	class basic_simd_processor
+	{
+	private:
+		SimdBackend processor_;
 
-  template <typename SimdBackend>
-  struct basic_simd;
-}
+		enum opcode
+		{
+			bad,
+			add,
+			mul,
+			div,
+		};
+
+	public:
+		basic_simd_processor()			= default;
+		virtual ~basic_simd_processor() = default;
+
+		basic_simd_processor& operator=(const basic_simd_processor&) = delete;
+		basic_simd_processor(const basic_simd_processor&)			 = delete;
+
+		typename SimdBackend::Register& call(const opcode& op, typename SimdBackend::Register& lhs, typename SimdBackend::Register& rhs)
+		{
+			switch (op)
+			{
+			case add:
+				return processor_.add(lhs, rhs);
+			case mul:
+				return processor_.mul(lhs, rhs);
+			case div:
+				return processor_.div(lhs, rhs);
+			default:
+				break;
+			}
+			return SimdBackend::Register::bad;
+		}
+
+		std::basic_string<char> isa()
+		{
+			return processor_.isa();
+		}
+	};
+} // namespace ocl::snu::simd
