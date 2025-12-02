475659b40c8a8d8e3c525bcad46091669ecf265f
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-02 02:39:47 -0500

    chore: unique_socket API improvements, and unit test updates.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- tests/network_basic/network_basic_test.cc
+++ tests/network_basic/network_basic_test.cc
@@ -14,7 +14,7 @@ TEST(NetworkTest, BasicNetworkTransmit)
 {
 	ocl::net::unique_socket sock = ocl::net::unique_socket::make_socket<8000>(ocl::net::unique_socket::local_address_ip4, false);
 	std::basic_string<char> buf_dst = "HELLO, WORLD!";
-	EXPECT_TRUE(sock.read(buf_dst.c_str(), buf_dst.size()).bad == false);
+	EXPECT_TRUE(sock.read_server_buffer(buf_dst.data(), buf_dst.size()).bad() == false);
 }
 
 /// @brief Basic Receive test
@@ -22,7 +22,7 @@ TEST(NetworkTest, BasicNetworkReceive)
 {
 	ocl::net::unique_socket sock = ocl::net::unique_socket::make_socket<8000>(ocl::net::unique_socket::local_address_ip4, true);
 
-	EXPECT_TRUE(!sock.bad);
+	EXPECT_TRUE(!sock.bad());
 
 	std::basic_string<char> buf_dst;
 	buf_dst.reserve(512);
@@ -30,11 +30,11 @@ TEST(NetworkTest, BasicNetworkReceive)
 	auto buf = buf_dst.data();
 	auto sz = buf_dst.size();
 
-	EXPECT_FALSE(!sock.read(buf, sz).bad);
+	EXPECT_FALSE(!sock.read_server_buffer(buf, sz).bad());
 }
 
 TEST(NetworkTest, BasicNetworkConstruct)
 {
 	auto socket = ocl::net::unique_socket::make_socket<8000>(ocl::net::unique_socket::local_address_ip4, true);
-	EXPECT_TRUE(!socket.bad);
+	EXPECT_TRUE(!socket.bad());
 }
