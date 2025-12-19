6471853426716df7f5b804649d0cf74df340dfe5
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-19 19:10:54 +0100

    hotpatch: Wrong preprocessor macro cond.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/ocl/is_same.hpp
+++ include/ocl/is_same.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#if __cplusplus <= 202301L
+#if __cplusplus < 202301L
 #error "This header has been deprecated, it now redirects to equiv.hpp"
 #else
 #warning "This header has been deprecated, it now redirects to equiv.hpp"
 #endif 
 
 #include <ocl/equiv.hpp>
-
